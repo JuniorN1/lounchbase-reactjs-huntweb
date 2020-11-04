52c465f46769661362a88ff97965bf552532fc74
JuniorN1 <junior_e.a.o@hotmail.com>
2020-11-04 20:54:50 -0300

    buscando dados da api rest

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React,{Component} from 'react';
 import Header from './components/Header';
+import Main from './pages/main';
 import './styles.css';
 const App = () =>(
   <div className="App">
     <Header />
+    <Main/>
   </div>
 )
 
