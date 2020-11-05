013952661a1c06260ef3d3c4e419ec7af2c47d4d
JuniorN1 <junior_e.a.o@hotmail.com>
2020-11-04 21:18:27 -0300

    personalizando lista

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,7 +1,6 @@
 import React,{Component} from 'react';
-
-
 import api from '../../services/api';
+import './styles.css';
 export default class Main extends Component{
     state = {
         products:[]
@@ -15,10 +14,17 @@ export default class Main extends Component{
         this.setState({products:response.data.docs})
     }
     render () {
+        const {products} = this.state;
         return(
-            <div className="product-liest">
-                {this.state.products.map(product=>(
-                    <h2 key={product._id}>{product.title}</h2>
+            <div className="product-list">
+                {products.map(product=>(
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+                        <a href="">Acessar</a>
+
+                    </article>
+                    
                 ))}
 
 
